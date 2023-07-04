175b5a5f620a8ba64b4f5c7254aab6e0d676ffd8
Ranga Senal Fernando <ranga.fernando@gmail.com>
2023-07-04 21:30:35 +1000

    Update readme

--- Me.Package.Dapr/obj/Debug/net7.0/Me.Package.Dapr.AssemblyInfo.cs
+++ Me.Package.Dapr/obj/Debug/net7.0/Me.Package.Dapr.AssemblyInfo.cs
@@ -10,7 +10,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Me.Package.Dapr")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("My Package")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
